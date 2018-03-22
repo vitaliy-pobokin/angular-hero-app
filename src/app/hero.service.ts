33b038cfa82ad988b6bae8543e851d618ec1d4bd
Vitaliy Pobokin <pbk.vitaliy@gmail.com>
2018-03-22 16:11:51 +0300

    Show message when heroes fetched

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,6 +11,8 @@ export class HeroService {
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    // Todo: send message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
