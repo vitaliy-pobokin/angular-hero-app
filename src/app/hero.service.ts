e968936e35d56b1287d2b6ba1cce78fa70d45780
Vitaliy Pobokin <pbk.vitaliy@gmail.com>
2018-03-22 15:55:07 +0300

    Added MessageService service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { MessageService } from './message.service';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     return of(HEROES);
