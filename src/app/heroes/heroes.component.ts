cc6002e49c948cc0063936febb0c3c3f9f10797f
Vitaliy Pobokin <pbk.vitaliy@gmail.com>
2018-03-22 14:48:39 +0300

    Added selected hero div

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,14 +11,14 @@ export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
