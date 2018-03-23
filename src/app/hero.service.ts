4c60b2f4f5c533e1d0912d3f587ecb6eae2c99ed
Vitaliy Pobokin <pbk.vitaliy@gmail.com>
2018-03-23 15:59:02 +0300

    addHero() method added

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -39,10 +39,19 @@ export class HeroService {
     );
   }
 
+  /** PUT: update the hero on the server */
   updateHero(hero: Hero): Observable<any> {
     return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
       tap(_ => this.log(`updated hero id=${hero.id}`)),
       catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
     );
   }
 
